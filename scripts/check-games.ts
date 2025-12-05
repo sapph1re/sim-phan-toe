261c42989fffe8aa33c3e504bc09a40ad4319c39
sapph1re <dev.romanv@gmail.com>
2025-12-05 16:09:42 -0300

    bugfix

--- scripts/check-games.ts
+++ scripts/check-games.ts
@@ -1,11 +1,11 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const contract = await ethers.getContractAt('SimPhanToe', '0xda70dc4AA88DEcD0edE470c3481b67D0D8c0B03F');
-  console.log('Game count:', (await contract.gameCount()).toString());
-  console.log('Open games:', await contract.getOpenGames());
-  const player = '0x6fe0E8BDAC611E9DF3334cDae26062bA621C434E';
-  console.log('Player games:', await contract.getGamesByPlayer(player));
+  const contract = await ethers.getContractAt("SimPhanToe", "0xda70dc4AA88DEcD0edE470c3481b67D0D8c0B03F");
+  console.log("Game count:", (await contract.gameCount()).toString());
+  console.log("Open games:", await contract.getOpenGames());
+  const player = "0x6fe0E8BDAC611E9DF3334cDae26062bA621C434E";
+  console.log("Player games:", await contract.getGamesByPlayer(player));
 }
 
 main().catch(console.error);
